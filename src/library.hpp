a6011fda6f6af0486e257e068b7af07cb9c03887
Tosiekdev <antonizieciak@gmail.com>
2024-07-19 19:01:31 +0200

    Created parsing value for x, Extracted functions to structs. Named operators.

--- src/library.hpp
+++ src/library.hpp
@@ -3,15 +3,21 @@
 
 #include <lexy/dsl.hpp>
 #include <lexy/callback.hpp>
+#include <lexy/token.hpp>
 
-namespace az { namespace { namespace grammar {
-    namespace dsl = lexy::dsl;
-
+namespace az {
     struct Number {
         Number(const std::string& i, const std::optional<std::string>& f) :
-            value(f ? std::stod(i+"."+*f) : std::stod(i)) {}
+                value(f ? std::stod(i+"."+*f) : std::stod(i)) {}
         double value;
     };
+
+    struct X {
+        double value;
+    };
+
+    namespace { namespace grammar {
+    namespace dsl = lexy::dsl;
 
     struct number {
         struct integer {
@@ -29,18 +35,52 @@ namespace az { namespace { namespace grammar {
         static constexpr auto value = lexy::construct<Number>;
     };
 
+    struct x {
+        static constexpr auto rule = dsl::lit_c<'x'>;
+        static constexpr auto value = lexy::callback<X>([](){ return X{}; });
+    };
+
+    constexpr auto op_plus  = dsl::op(dsl::lit_c<'+'>);
+    constexpr auto op_minus = dsl::op(dsl::lit_c<'-'>);
+    constexpr auto op_mul  = dsl::op(dsl::lit_c<'*'>);
+    constexpr auto op_div = dsl::op(dsl::lit_c<'/'>);
+    constexpr auto op_pow  = dsl::op(dsl::lit_c<'^'>);
     struct production : lexy::expression_production {
+        struct sin {
+            static constexpr auto rule = dsl::lit<"sin"> >> dsl::parenthesized(dsl::p<production>);
+        };
+
+        struct cos {
+            static constexpr auto rule = dsl::lit<"cos"> >> dsl::parenthesized(dsl::p<production>);
+        };
+
+        struct tan {
+            static constexpr auto rule = dsl::lit<"tan"> >> dsl::parenthesized(dsl::p<production>);
+        };
+
+        struct cot {
+            static constexpr auto rule = dsl::lit<"cot"> >> dsl::parenthesized(dsl::p<production>);
+        };
+
+        struct sqrt {
+            static constexpr auto rule = dsl::lit<"sqrt"> >> dsl::parenthesized(dsl::p<production>);
+        };
+
+        struct cbrt {
+            static constexpr auto rule = dsl::lit<"cbrt"> >> dsl::parenthesized(dsl::p<production>);
+        };
+
         static constexpr auto whitespace = dsl::ascii::space;
         static constexpr auto atom= [] {
-            auto integer= dsl::digits<>.no_leading_zero();
-            auto fraction= dsl::digits<>;
-            auto functionKw = dsl::lit<"sin">
-                    | dsl::lit<"cos"> | dsl::lit<"tan"> | dsl::lit<"cot"> | dsl::lit<"sqrt"> | dsl::lit<"cbrt">;
-            return functionKw >> dsl::parenthesized(dsl::recurse<production>) | integer >> dsl::if_(dsl::period >> fraction) | dsl::lit_c<'x'>;
+//            auto functionKw =
+//                    | dsl::lit<"cos"> | dsl::lit<"tan"> | dsl::lit<"cot"> | dsl::lit<"sqrt"> | dsl::lit<"cbrt">;
+            auto function =
+                    dsl::p<sin> | dsl::p<cos> | dsl::p<tan> | dsl::p<cot> | dsl::p<sqrt> | dsl::p<cbrt>;
+            return function | dsl::p<number> | dsl::p<x>;
         }();
 
         struct power : dsl::infix_op_left {
-            static constexpr auto op = dsl::op(dsl::lit_c<'^'>);
+            static constexpr auto op = op_pow;
             using operand = dsl::atom;
         };
 
@@ -50,12 +90,12 @@ namespace az { namespace { namespace grammar {
         };
 
         struct product : dsl::infix_op_left {
-            static constexpr auto op= dsl::op(dsl::lit_c<'*'>) / dsl::op(dsl::lit_c<'/'>);
+            static constexpr auto op = op_mul / op_div;
             using operand = prefix;
         };
 
         struct sum : dsl::infix_op_left {
-            static constexpr auto op= dsl::op(dsl::lit_c<'+'>) / dsl::op(dsl::lit_c<'-'>);
+            static constexpr auto op= op_plus / op_minus;
             using operand = product;
         };
 
--- test/test.cpp
+++ test/test.cpp
@@ -76,7 +76,7 @@ TEST(GrammarTest, FunctionKeywords) {
     EXPECT_TRUE(lexy::match<az::grammar::production>(cubeRoot));
 }
 
-TEST(ParsingTest, Parse) {
+TEST(ParsingTest, ParseNumber) {
     auto integer = lexy::zstring_input("5");
     auto decimal = lexy::zstring_input("3.14");
     auto integerResult = lexy::parse<az::grammar::number>(integer, lexy_ext::report_error);
@@ -85,4 +85,11 @@ TEST(ParsingTest, Parse) {
     EXPECT_DOUBLE_EQ(integerResult.value().value, 5.0);
     ASSERT_TRUE(decimalResult.has_value());
     EXPECT_DOUBLE_EQ(decimalResult.value().value, 3.14);
+}
+
+TEST(ParsingTest, ParseX) {
+    auto var = lexy::zstring_input("x");
+    auto result = lexy::parse<az::grammar::x>(var, lexy_ext::report_error);
+    ASSERT_TRUE(result.has_value());
+    EXPECT_EQ(result.value().value, 0);
 }
